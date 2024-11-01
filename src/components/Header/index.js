32f039f74f7e07ae1b8f9fa354890825c621d28e
MatheusLimaBarbosa <140213823+MatheusLimaBarbosa@users.noreply.github.com>
2024-10-31 21:33:54 -0300

    Atualizando CSS e SCSS

---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import logosvg from '../../svg/blog-logo.svg'
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <div className="logo">
+                    <img src={logosvg} alt=""/>
+                </div>
+                <div className="search">
+                    
+                </div>
+                <div className="menu">
+                    
+                </div>
+            </header>
+        </>
+    );
+}
+
+export default Header;
